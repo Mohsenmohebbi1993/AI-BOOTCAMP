39052ed2ce6ddeb63be077de5ddf255e2ac67907
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-11 23:08:27 +0330

    make one package file

---
+++ notebooks/Training & Education/my_package/mathematics.py
@@ -0,0 +1,9 @@
+
+def sum_test(first_number:float, second_number:float)->float:
+    """ Adds tow Number and return the result """
+    return first_number + second_number
+
+
+def sub_test(first_number:float, second_number:float)->float:
+    """ Subtracts tow Number and return the result """
+    return first_number - second_number
