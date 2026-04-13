df3bbe0d81338ce6a08b0160e425083126b6eb39
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-13 23:03:17 +0330

    weite comment

--- notebooks/Project/1. HW-L02-01-Python/1.py
+++ notebooks/Project/1. HW-L02-01-Python/1.py
@@ -1,4 +1,5 @@
 # Print the numbers
+# Task is: 
 # Write a program that receives a number greater than 3 from the user
 # If the number is odd, it should print all odd numbers from 1 up to that number 
 # If the number is even, it should print all even numbers from 2 up to that number
