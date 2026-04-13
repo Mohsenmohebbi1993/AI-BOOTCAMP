d9abcee06404599369350e1e584b1a93829cb33b
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-13 21:58:51 +0330

    Print the numbers

--- notebooks/Project/1. HW-L02-01-Python/1.py
+++ notebooks/Project/1. HW-L02-01-Python/1.py
@@ -1,1 +1,7 @@
-print("hello world")
+# Print the numbers
+# Write a program that receives a number greater than 3 from the user
+# If the number is odd, it should print all odd numbers from 1 up to that number 
+# If the number is even, it should print all even numbers from 2 up to that number
+# If the user's input is invalid, an appropriate error message should be displayed
+
+number = input("Enter a Number: ")
