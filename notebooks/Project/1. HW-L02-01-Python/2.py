5e942f6407b3bf311910cba2e9f9d536d756aa95
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-13 23:20:40 +0330

    function

--- notebooks/Project/1. HW-L02-01-Python/2.py
+++ notebooks/Project/1. HW-L02-01-Python/2.py
@@ -1,5 +1,19 @@
+# Vowel counter
+#task is:
 # Write a program that receives a string from the user
 # and counts the number of vowels (a, e, i, o, u),
 # ignoring case sensitivity.
 
 
+def count_vowels(text:str)->int:
+    """ 
+        The function count_vowels takes a text and counts the number of vowels 
+    """
+
+    VOWELS = ["a", "e", "i", "o", "u"]
+    TEXT_LOWER = TEXT.lower()
+    counter = 0
+    for i in TEXT_LOWER:
+        if i in VOWELS:
+            counter += 1
+    print(f"Number of vowels is {counter}") 
