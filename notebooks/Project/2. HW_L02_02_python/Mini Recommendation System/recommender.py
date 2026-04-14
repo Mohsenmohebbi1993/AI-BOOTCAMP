6940a806cb19a5a475a50f24f5285668e1aa79d0
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-14 21:30:05 +0330

    Checking_json_file

---
+++ notebooks/Project/2. HW_L02_02_python/Mini Recommendation System/recommender.py
@@ -0,0 +1,32 @@
+# Mini Recommendation System
+
+#-----------------------
+# package import:
+import os
+
+#-----------------------
+print("----------------start Code----------------")
+
+# Function:
+
+def Checking_json_file(PATH:str):
+    """ **Checking_json_file**   
+    Chacke json file as `users.json`   
+    In this directory, check whether the file `users.json` exists.    
+    If it does not exist, create the JSON file using the code in `json_maker.py`.  
+    """
+    # PATH = r".\notebooks\Project\2. HW_L02_02_python\Mini Recommendation System"
+
+    LIST_FILE = os.listdir(PATH) # list name of file in path
+
+    if not "users.json" in LIST_FILE: # chacke file as `users.json`
+        import json_maker # use my mudel, make `users.json` default
+        json_maker.make_json_file()
+        print("Creat users.json file in this Directory")
+    else:
+        print("Find users.json in this Directory")
+
+
+PATH = r".\notebooks\Project\2. HW_L02_02_python\Mini Recommendation System"
+Checking_json_file(PATH)
+
