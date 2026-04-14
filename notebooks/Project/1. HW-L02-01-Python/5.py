d059729308d3fed24ce35d2e20349c5f37c99197
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-14 19:14:04 +0330

    salve 3 form

--- notebooks/Project/1. HW-L02-01-Python/5.py
+++ notebooks/Project/1. HW-L02-01-Python/5.py
@@ -2,8 +2,44 @@
 # task is:
 # give one sentence and reverse
 
+
+def reverse_the_sentence_1(SENTEMCE:str):
+    """ **Reverse the sentence**  
+        give one sentence and reverse
+    """
+    SENTEMCE = SENTEMCE.replace(".", " ").replace("  ", " ").strip()
+    LIST_SENTEMCE = SENTEMCE.split()
+    REVERSED_SENTEMCE = LIST_SENTEMCE[::-1]
+    print(*REVERSED_SENTEMCE)
+
+# or ---------------------
+def reverse_the_sentence_2(SENTEMCE:str):
+    """ **Reverse the sentence**  
+        give one sentence and reverse
+    """
+    SENTEMCE = SENTEMCE.replace(".", " ").replace("  ", " ").strip()
+    LIST_SENTEMCE = SENTEMCE.split()
+    for i in range(len(LIST_SENTEMCE)-1, -1, -1): # range(10 , 0, -1) does not include 0
+        print(LIST_SENTEMCE[i], end=" ")
+    print() # last print has end=" ",don't want the next print to stick to it
+
+# or 
+def reverse_the_sentence_3(SENTEMCE:str):
+    """ **Reverse the sentence**  
+        give one sentence and reverse
+    """
+    SENTEMCE = SENTEMCE.replace(".", " ").replace("  ", " ").strip()
+    LIST_SENTEMCE = SENTEMCE.split()
+    for i in range(0, len(LIST_SENTEMCE)):
+        # (len(LIST_SENTEMCE) - i - 1) index start 0 to len -1
+        print(LIST_SENTEMCE[(len(LIST_SENTEMCE) - i - 1)], end=" ")
+    print() # last print has end=" ",don't want the next print to stick to it
+
+
+
+
+
 SENTEMCE = "Data science is fun"
-
-for i in (SENTEMCE):
-    print(i)
-
+reverse_the_sentence_1(SENTEMCE)
+reverse_the_sentence_2(SENTEMCE)
+reverse_the_sentence_3(SENTEMCE)
