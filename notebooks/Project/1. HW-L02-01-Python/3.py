3e21136f4d2bdb2f93b664402d8f440cb83ed956
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-14 17:55:24 +0330

    test

--- notebooks/Project/1. HW-L02-01-Python/3.py
+++ notebooks/Project/1. HW-L02-01-Python/3.py
@@ -4,14 +4,22 @@
 # counts the frequency of each word in it,
 # and stores the result in a dictionary.
 
+def dictionary(TEXT):
+    TEXT_LOWER = TEXT.lower()
+    LIST_TEXT = TEXT_LOWER.split(" ")
+    LIST_TEST_UNIQUE = list(set(LIST_TEXT))
+    dictionary = {}
+
+    for word in LIST_TEST_UNIQUE:
+        counter = 0
+        for word_in_text in LIST_TEXT:
+            if word == word_in_text:
+                counter +=1
+        dictionary[word] = counter
+
+    print(dictionary)
+
+
 TEXT = "Data science makes data useful"
-
-
-TEXT_LOWER = TEXT.lower()
-LIST_TEXT = TEXT_LOWER.split(" ")
-LIST_TEST_UNIQUE = list(set(LIST_TEXT))
-print(LIST_TEST_UNIQUE)
-
-
-
+dictionary(TEXT)
 
