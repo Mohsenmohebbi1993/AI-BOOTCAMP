be4b8be06c5241a6334a25016fe8f02c9ae66189
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-14 18:20:20 +0330

    Calculate the average

---
+++ notebooks/Project/1. HW-L02-01-Python/4.py
@@ -0,0 +1,14 @@
+# Calculate the average
+#task is:
+#Write a program that calculates the user's GPA based on the list below.
+# First, ask the user for the number of courses,
+# then receive the grades for those courses,
+# and finally calculate the average of these grades and convert it to a GPA according to the table below.
+
+# A : 20 - 18
+# B : 17.99 - 15
+# C : 14.99 - 12
+# F : 11.99 - 0
+
+print("First, enter the number of courses")
+NUMBER_OF_COURSES = int(input("Enter number of courses: "))
