a61ba1e35d66c42740220cddf6a6d25b6e1dee30
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-14 18:08:58 +0330

    make comment

--- notebooks/Project/1. HW-L02-01-Python/3.py
+++ notebooks/Project/1. HW-L02-01-Python/3.py
@@ -4,15 +4,17 @@
 # counts the frequency of each word in it,
 # and stores the result in a dictionary.
 
-def dictionary(TEXT):
+def dictionary(TEXT:str)->dict:
+    """ The dictionary function counts the number of words in a text """
+
     TEXT_LOWER = TEXT.lower()
     # replace "." to " "
     # replace "  "tow space to " " one space
     #strip first and end space
     TEXT_LOWER = TEXT_LOWER.replace(".", " ").replace("  ", " ").strip()
-    LIST_TEXT = TEXT_LOWER.split(" ")
-    LIST_TEST_UNIQUE = list(set(LIST_TEXT))
-    dictionary = {}
+    LIST_TEXT = TEXT_LOWER.split(" ") # make one list from each word
+    LIST_TEST_UNIQUE = list(set(LIST_TEXT)) # Unique word
+    dictionary = {} # empty dic
 
     for word in LIST_TEST_UNIQUE:
         counter = 0
