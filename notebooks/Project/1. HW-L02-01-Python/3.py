d1904f1e16fe528960b20687332c6390233d0d2f
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-14 17:48:04 +0330

    make list of UNIQUE

--- notebooks/Project/1. HW-L02-01-Python/3.py
+++ notebooks/Project/1. HW-L02-01-Python/3.py
@@ -3,3 +3,15 @@
 # Write a program that takes a string as input,
 # counts the frequency of each word in it,
 # and stores the result in a dictionary.
+
+TEXT = "Data science makes data useful"
+
+
+TEXT_LOWER = TEXT.lower()
+LIST_TEXT = TEXT_LOWER.split(" ")
+LIST_TEST_UNIQUE = list(set(LIST_TEXT))
+print(LIST_TEST_UNIQUE)
+
+
+
+
