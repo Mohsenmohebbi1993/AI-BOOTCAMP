34ae728ce32af291c71bfc7a321a5a39ab72aff3
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-19 21:44:12 +0330

    create class circle

---
+++ notebooks/Training & Education/1. OOP Project/circle.py
@@ -0,0 +1,10 @@
+from base import Shape
+
+class Circle(Shape):
+
+    def __init__(self, redius):
+        self.redius = redius
+
+    
+    def area(self):
+        return self.redius * self.redius * 3.14159
