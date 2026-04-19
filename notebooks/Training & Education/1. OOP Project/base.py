c6b5f877011f320427eb8d7a05d9371d2317e195
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-19 21:31:33 +0330

    creat base class

--- notebooks/Training & Education/1. OOP Project/base.py
+++ notebooks/Training & Education/1. OOP Project/base.py
@@ -1,1 +1,13 @@
+
 from abc import ABC, abstractmethod
+# It is used to utilize the capability of abstract classes in Python.
+
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        """ Classes that inherit from this must implement the `area` method"""
+        pass
+
+
