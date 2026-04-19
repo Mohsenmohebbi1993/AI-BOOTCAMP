78dfdac1929d63cb984b26d5a97c130b57c8eddc
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-19 21:52:01 +0330

    run all class

---
+++ notebooks/Training & Education/1. OOP Project/main.py
@@ -0,0 +1,8 @@
+from rectangle import Rectangle
+from circle import Circle
+
+list_of_shape = [(Rectangle, (3, 4)), (Circle, (5,))]
+
+for ctor, arge in list_of_shape:
+    shape = ctor(*arge)
+    print(f"The area of The {shape.__class__.__name__} is : {shape.area():.2f}")
