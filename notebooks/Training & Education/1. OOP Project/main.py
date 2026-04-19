4e380d36162b5dbf829f4628c6809ae51230f294
Mohsenmohebbi1993 <mohsen_mohebbi@ymail.com>
2026-04-19 22:02:03 +0330

    end

--- notebooks/Training & Education/1. OOP Project/main.py
+++ notebooks/Training & Education/1. OOP Project/main.py
@@ -1,8 +1,15 @@
 from rectangle import Rectangle
 from circle import Circle
 
-list_of_shape = [(Rectangle, (3, 4)), (Circle, (5,)), (Rectangle, (-2, 5))]
+list_of_shape = [(Rectangle, (3, 4)), (Circle, (5,)), (Rectangle, (-2, 5)), (Circle, (-10,))]
 
 for ctor, arge in list_of_shape:
-    shape = ctor(*arge)
-    print(f"The area of The {shape.__class__.__name__} is : {shape.area():.2f}")
+    try:
+        shape = ctor(*arge)
+        print(f"The area of The {shape.__class__.__name__} is : {shape.area():.2f}")
+
+    except ValueError as e:
+        print(f"Error create {ctor.__name__} with arge {arge}: {e}")
+        continue
+
+    
